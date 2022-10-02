f2ea558d4ddaab711fbe7cd309ba9252b1a4e4d8
bd240897 <bd240897@yandex.ru>
2022-10-02 23:44:21 +0300

    fixed DB name v2

--- Dmitriibox/settings/test.py
+++ Dmitriibox/settings/test.py
@@ -12,7 +12,7 @@ ALLOWED_HOSTS = ['*']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'DmitriiBox',
+        'NAME': 'dmitriibox',
         'USER': 'amid',
         'PASSWORD': '1234qwer',
         'HOST': 'localhost',
